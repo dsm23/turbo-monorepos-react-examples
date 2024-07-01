5f6eaffb5fdba8a7a0e08d8b9fa76e880276cc9b
David Murdoch <dsmurdoch357@gmail.com>
2024-07-01 11:08:17 +0100

    feat(accordions): add accordions example app

---
+++ apps/accordions/src/components/BarebonesAccordion.tsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import type { FunctionComponent } from "react";
+import { Transition } from "@headlessui/react";
+import { Button } from "./Button";
+import ChevronDownIcon from "./ChevronDownIcon";
+import type { Data } from "~/types";
+import cn from "~/utils";
+
+type Props = {
+  data: Data[];
+};
+
+const Accordian: FunctionComponent<Props> = ({ data }) => {
+  const [selected, setSelected] = useState<Data["id"] | null>(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState<Data["id"][]>([]);
+
+  function handleSingleSelection(getCurrentId: Data["id"]) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  function handleMultiSelection(getCurrentId: Data["id"]) {
+    if (!multiple.includes(getCurrentId)) {
+      setMultiple((prevMultiple) => [...prevMultiple, getCurrentId]);
+      return;
+    }
+
+    setMultiple((prevMultiple) => {
+      const index = prevMultiple.indexOf(getCurrentId);
+
+      return [
+        ...prevMultiple.slice(0, index),
+        ...prevMultiple.slice(index + 1),
+      ];
+    });
+  }
+
+  const isOpen = (id: Data["id"]) =>
+    enableMultiSelection ? multiple.includes(id) : selected === id;
+
+  return (
+    <>
+      <Button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        {enableMultiSelection ? "Dis" : "En"}able Multi Selection
+      </Button>
+      <div>
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="border-b">
+              <button
+                className="flex w-full flex-1 items-center justify-between py-4 font-medium transition-all hover:underline"
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+              >
+                <h3>{dataItem.question}</h3>
+                <ChevronDownIcon
+                  className={cn(
+                    "h-4 w-4 shrink-0 transition-transform duration-200",
+                    {
+                      ["rotate-180"]: isOpen(dataItem.id),
+                    },
+                  )}
+                />
+              </button>
+
+              <Transition show={isOpen(dataItem.id)}>
+                <div
+                  className={cn(
+                    "grid transition-[grid-template-rows] duration-[200] ease-out motion-reduce:transition-none",
+                    "data-[enter]:data-[closed]:grid-rows-[0fr] data-[enter]:grid-rows-[1fr]",
+                    "data-[leave]:data-[closed]:grid-rows-[0fr] data-[leave]:grid-rows-[1fr]",
+                  )}
+                >
+                  <div className="overflow-hidden">
+                    <div className="text-sm">
+                      <div className="pb-4 pt-0">{dataItem.answer}</div>
+                    </div>
+                  </div>
+                </div>
+              </Transition>
+            </div>
+          ))
+        ) : (
+          <div>No data found !</div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Accordian;
---
+++ apps/accordions/src/components/ChevronDownIcon.tsx
@@ -0,0 +1,24 @@
+import type { FunctionComponent, SVGAttributes } from "react";
+import cn from "~/utils";
+
+type Props = SVGAttributes<SVGSVGElement>;
+
+const ChevronDownIcon: FunctionComponent<Props> = ({ className, ...props }) => (
+  <svg
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className={cn("size-6", className)}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m19.5 8.25-7.5 7.5-7.5-7.5"
+    />
+  </svg>
+);
+
+export default ChevronDownIcon;
