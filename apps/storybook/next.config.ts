495c55c445effe2c98c0bdb76a4847a08b3c4ea9
David Murdoch <dsmurdoch357@gmail.com>
2025-12-02 14:52:16 +0000

    chore(deps): update npm dependencies

--- apps/storybook/next.config.ts
+++ apps/storybook/next.config.ts
@@ -3,13 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  eslint: {
-    // !! WARN !!
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    // !! WARN !!
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
